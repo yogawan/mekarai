608a639280892dc6aa8e28822cdd4616013af7bf
Yogaone <yogawanadityapratama@gmail.com>
2025-06-08 13:45:46 +0700

    Restore UI: From BanjirAI to JawirAI

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -2,18 +2,13 @@ import ProtectedImage from "@/components/ProtectedImage";
 
 const ChatHeader = () => (
   <div className="pl-5 pr-5 pb-5 bg-none">
-    {/* <ProtectedImage
+    <ProtectedImage
       src="/brand/logo.png"
       alt="logo"
       className="h-16 mb-3"
-    /> */}
-    <p className="text-3xl font-thin text-white">Tanya AI</p>
-    <p className="text-xl font-thin text-white/75">
-      {/* <u><a href="https://github.com/yogawan/jawiraiv1.6.3">Open Source</a></u>
-      <i className="text-white">User Interface</i>
-      to interact with AI Model */}
-      Tanya apa saja seputar banjir.
-    </p>
+    />
+    <p className="text-3xl font-thin text-white">ꦗꦮꦶꦫꦆꦌ (JawirAI)</p>
+    <p className="text-xl font-thin text-white/75"><u><a href="https://github.com/yogawan/jawiraiv1.6.3">Open Source</a></u> <i className="text-white">User Interface</i> to interact with AI Model</p>
   </div>
 );
 
